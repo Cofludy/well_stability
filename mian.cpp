631a45fb1de80307795610649013838a6c48bf6a
Cofludy <cofludy@gmail.com>
2016-11-03 17:09:32 +0800

    Create mian.cpp

---
+++ mian.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "parameter.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    parameter para;
+    para.input();
+    return 0;
+}
+
+/*
+    write(*,*)  "debug=== ", nprop,props(1,1),props(1,7)
+    read(*,*)
+*/
